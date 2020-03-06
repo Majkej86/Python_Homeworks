e4b68baea24143b34a449f01b50be32180c00948
Michal Marchanski <m.marchanski@gmail.com>
2020-03-06 17:10:50 +0100

    First Commit

---
+++ wizytowka.py
@@ -0,0 +1,7 @@
+nazwisko = "Michał Marchański"
+tytuł = "Karate Miszcz"  
+
+print("+-" + "-" * (len(nazwisko)) + "-+\n" 
+    + "|" + " " + nazwisko + " " + "|\n"
+    + "|" + (" " * 3) + tytuł + (" " * 3) + "|\n"
+    + "+-" + "-" * (len(nazwisko)) + "-+")
